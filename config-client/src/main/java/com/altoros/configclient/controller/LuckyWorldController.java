c969c85aef2ad87f00a135996bc22f0656e8d77e
Uladzislau Kaminski <kaminski.uladzislau@gmail.com>
2018-11-16 12:06:47 +0300

    Add message bus config

--- config-client/src/main/java/com/altoros/configclient/controller/LuckyWorldController.java
+++ config-client/src/main/java/com/altoros/configclient/controller/LuckyWorldController.java
@@ -1,13 +1,15 @@
 package com.altoros.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class LuckyWorldController {
 
-    @Value("${lucky-world}")
+    @Value("${word-config.lucky-world}")
     private String luckyWord;
 
     @GetMapping("lucky-world")
